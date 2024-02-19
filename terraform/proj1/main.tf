53b938ddb2231f30dab70de7c80997e1b660ed7d
Shane Holder <shane.holder@pobox.com>
2024-02-19 17:36:10 -0600

    wip

--- terraform/proj1/main.tf
+++ terraform/proj1/main.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
+
+resource "azurerm_resource_group" "rg2" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id + "_xyzzy"
+}
