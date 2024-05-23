a1ec911ec833600d36a300a62f12e11e40e27bc6
highwinds-io <46505675+highwinds-io@users.noreply.github.com>
2024-05-23 13:42:46 -0500

    Update main.tf

--- terraform/proj1/main.tf
+++ terraform/proj1/main.tf
@@ -2,6 +2,10 @@ resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
 
+resource "random_string" "random" {
+  length           = 8
+  special          = false
+}
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = format("%s_%s", random_pet.rg_name.id, var.environment)
@@ -13,3 +17,7 @@ resource "azurerm_resource_group" "rg2" {
   name     = format("%s_%s", "fred", var.environment)
 }
 
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = format("%s_%s", random_string.random.id, var.environment)
+}
