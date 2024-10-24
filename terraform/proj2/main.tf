0c0670d96b82091d3903979cfd65bb9420a899e3
Shane Holder <shane.holder@pobox.com>
2024-10-24 09:57:28 -0500

    Mono-repo-support (#46)
    feat: support mono repo with release please

---
+++ terraform/proj2/main.tf
@@ -0,0 +1,9 @@
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = format("%s_%s", random_pet.rg_name.id, var.environment)
+}
+
